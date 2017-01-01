cd3c316769dd386d47208f89f21a78c885225e85
Kevin Lewis <kevin@jellybabi.es>
2017-01-01 12:09:31 +0000

    Moved keys to their oen file

---
+++ options.js
@@ -0,0 +1,11 @@
+var SFW_LOWER_LIMIT = 0.8;
+var FORM_ID = "clarifai-nsfw"
+var FILE_CLASS = "nsfw-check";
+
+function clarifaiCheckPass() {
+  // PUT YOUR DESIRED ACTIONS HERE
+}
+
+function clarifaiCheckFail() {
+  // PUT YOUR DESIRED ACTIONS HERE
+}
